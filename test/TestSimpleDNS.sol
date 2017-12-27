b1a67614382736a3d11763346a61d2e7b80c92e5
joamag <joamag@hive.pt>
2017-12-27 16:16:17 +0000

    better structure

--- test/TestSimpleDNS.sol
+++ test/TestSimpleDNS.sol
@@ -4,10 +4,9 @@ import "truffle/Assert.sol";
 import "../contracts/SimpleDNS.sol";
 
 contract TestSimpleDNS {
-    function testSimpleDomain() {
-        SimpleDNS dns = SimpleDNS();
+    function testSimpleDomain() public {
+        SimpleDNS dns = new SimpleDNS();
         dns.addDomain("hello.com", "127.0.0.1");
-
-        Assert.equal(dns.getDomain("hello.com"), "127.0.0.1", "hello.com should be 127.0.0.1");
+        Assert.equals(dns.getDomain("hello.com"), "127.0.0.1", "hello.com should be 127.0.0.1");
     }
 }
